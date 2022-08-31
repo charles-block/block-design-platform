037c6ef09fad310374f1982184265eb3ade47355
cocowang22 <coco@blockrenovation.com>
2022-08-31 18:19:57 -0400

    Create RevitModels.js

---
+++ block-design/src/components/pages/content/bim/RevitModels.js
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/esm/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { ArrowRight } from 'react-bootstrap-icons';
+
+function RevitModels() {
+    return (
+        <Container>
+            <h2>
+                Revit Models
+            </h2><br />
+            <div style={{ overflowY: 'scroll', overflowX: 'hidden', height: '78vh' }}>
+                
+                <ListGroup as="ol">
+                    <ListGroup.Item action target="_blank" rel="noopener noreferrer" href="https://drive.google.com/drive/folders/19z8AY_N1oQ57GqDMPX2Ag6ZC2KuF9C4b"
+                        className="d-flex justify-content-between align-items-start"
+                    >
+                        <div className="ms-2 me-auto">
+                            <div className="fw-bold">Viatechnik Models <ArrowRight /></div>
+                        Families created and updated by Viatechnik.
+                        </div>
+                    </ListGroup.Item>
+                </ListGroup>
+            </div>
+        </Container>
+    );
+}
+
+export default RevitModels;
+
