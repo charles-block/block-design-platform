f477afc8ca5723bd7dd8df9bea4f69bfd2964d67
cocowang22 <coco@blockrenovation.com>
2022-09-13 00:14:41 -0400

    Adding host to server

--- server/index.js
+++ server/index.js
@@ -32,12 +32,13 @@ app.get('/', (req, res) => {
 //Store in env variable
 const CONNECTION_URL = 'mongodb+srv://block-design:JKKJKK333@cluster0.gzkip.mongodb.net/?retryWrites=true&w=majority';
 
+const host = '0.0.0.0';
 // hiroku will auto popuate env variabole called PORT
 const PORT = process.env.PORT || 5000;
 
 //connect to database
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => app.listen(PORT, host, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
 
